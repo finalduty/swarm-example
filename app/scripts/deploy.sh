f7a0514792b69ac3c25cb64235114f28c91336c1
Andy Dustin <andy.dustin@gmail.com>
2017-01-30 14:56:32 +1300

    Catchup

---
+++ app/scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eux
+
+deploy_dir_base="/deploy"
+deploy_app_name="example_app"
+deploy_colour="$(echo `curl 10.0.0.11/v1/kv/example-app/colour/test -H "Host: consul.local" 2>/dev/null | jq -r .[].Value  | base64 -d`)"
+active_colour="$(echo `curl 10.0.0.11/v1/kv/example-app/colour/active -H "Host: consul.local" 2>/dev/null | jq -r .[].Value  | base64 -d`)"
+deploy_dir_bottom="html"
+
+deploy_path="$deploy_dir_base/$deploy_app_name/$deploy_colour/$deploy_dir_bottom"
+
+if [ -d $deploy_path ]; then
+    echo "Deploying $deploy_app_name to $deploy_colour"
+    echo "Deploy Path: $deploy_path"
+    
+    rsync -triP ../src $deploy_path
+    
+else
+    echo "Something went wrong"   
+    exit 1
+fi
---
+++ app/scripts/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+EXITCODE=0
+
+[ `egrep -i 'hello' ../src/index.html &>/dev/null; echo $?` -eq 0 ] || EXITCODE=1
+
+[ $EXITCODE -eq 0 ] && echo "Tests Passed" || echo "Tests Failed D: ):"
+
+exit $EXITCODE
