64081d94fc5bae5ed2dac4b5391ce7b23e2053e3
Andy Dustin <andy.dustin@gmail.com>
2017-01-30 13:36:58 +1300

    Added output

--- test.sh
+++ test.sh
@@ -5,4 +5,6 @@ EXITCODE=0
 [ `egrep -i 'hello' app/example.txt &>/dev/null; echo $?` -eq 0 ] || EXITCODE=1
 [ `egrep 'FROM' app/Dockerfile &>/dev/null; echo $?` -eq 0 ] || EXITCODE=1
 
+[ $EXITCODE -eq 0 ] && echo "Tests Passed" || echo "Tests Failed D: ):"
+
 exit $EXITCODE
