82b3a1fe50fc072461493cd90eb0cda84de0c99b
Andy Dustin <andy.dustin@gmail.com>
2017-01-30 13:34:20 +1300

    working now

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 EXITCODE=0
 
-[ `egrep -i 'hello' app/example.txt` ] || EXITCODE=1
-[ `egrep 'FROM' app/Dockerfile` ] || EXITCODE=1
+[ `egrep -i 'hello' app/example.txt &>/dev/null; echo $?` -eq 0 ] || EXITCODE=1
+[ `egrep 'FROM' app/Dockerfile &>/dev/null; echo $?` -eq 0 ] || EXITCODE=1
 
 exit $EXITCODE
