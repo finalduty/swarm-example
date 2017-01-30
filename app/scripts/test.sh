8b086bbbbbea19717dba3dbb18742b62550fe44f
Andy Dustin <andy.dustin@gmail.com>
2017-01-30 14:59:34 +1300

    Changed relative path of test

--- app/scripts/test.sh
+++ app/scripts/test.sh
@@ -2,7 +2,7 @@
 
 EXITCODE=0
 
-[ `egrep -i 'hello' ../src/index.html &>/dev/null; echo $?` -eq 0 ] || EXITCODE=1
+[ `egrep -i 'hello' ./src/index.html &>/dev/null; echo $?` -eq 0 ] || EXITCODE=1
 
 [ $EXITCODE -eq 0 ] && echo "Tests Passed" || echo "Tests Failed D: ):"
 
