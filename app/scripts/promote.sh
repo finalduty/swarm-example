9c671db9559224a98427d4e1a1aa4e7c9b577d74
Andy Dustin <andy.dustin@gmail.com>
2017-01-30 17:31:51 +1300

    Actually added promote.sh this time

---
+++ app/scripts/promote.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -eu
+
+EXITCODE=0
+
+deploy_app_name="example_app"
+deploy_base_dir="/deploy"
+deploy_base_url="example.local"
+deploy_colour="$(echo `curl -s 10.0.0.11/v1/kv/example-app/test/colour -H "Host: consul.local" | jq -r .[].Value  | base64 -d`)"
+active_colour="$(echo `curl -s 10.0.0.11/v1/kv/example-app/active/colour -H "Host: consul.local" | jq -r .[].Value  | base64 -d`)"
+
+## Test that website works in staging
+[ `curl -sLI -o /dev/null -w "%{http_code}\n" 10.0.0.11 -H "Host: $deploy_colour.$deploy_base_url"` -eq 200 ] || EXITCODE=1
+[ `curl -sL 10.0.0.11 -H "Host: $deploy_colour.$deploy_base_url" | egrep -c "Hello"` -ne 0 ] || EXITCODE=1
+
+## Exit here if we failed any tests, 
+if [ $EXITCODE -gt 0 ]; then
+    echo "Post-Deploy Tests Failed"
+    exit $EXITCODE
+else
+    echo "Post-Deploy Tests Passed"
+fi
+
+## Swap backends for production and testing key/values in Consul
+[ `curl -X PUT -d "$active_colour" 10.0.0.11/v1/kv/example-app/test/colour -H "Host: consul.local"` ] || EXITCODE=2
+[ `curl -X PUT -d "$deploy_colour" 10.0.0.11/v1/kv/example-app/active/colour -H "Host: consul.local"` ] || EXITCODE=2
+
+## Call an adult if we fail here D:
+if [ $EXITCODE -gt 0 ]; then
+    echo "Promotion failed, oh crap D:"
+    exit $EXITCODE
+else
+    echo "Promotion successful"
+fi
+
+## If by this point we've passed the post-deploy tests and the promotion, we can update the binding for nginx
+## If it went bad, nothing has been updated here, but we'd probably be safe anyways
+
+echo '$1$Gik.T5b9$q9bzobpNYrkbdX//qcM11.' | ssh root@10.0.0.11 /etc/nginx/conf.d/example.switch
